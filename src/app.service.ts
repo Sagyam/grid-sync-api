75dc2794c3bc536651bfca13b1d88393fa3f73cc
Sagyam Thapa <sagyamthapa32@gmail.com>
2023-11-24 21:51:58 +0545

    📚 docs(app): Added docs generation pipeline

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+  /**
+   * Retrieves a greeting message.
+   *
+   * This method returns a string containing the greeting message 'Hello World!'.
+   *
+   * @returns {string} The greeting message.
+   */
   getHello(): string {
     return 'Hello World!';
   }
