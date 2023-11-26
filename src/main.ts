ae8868ed3866db5bf8e25e19fcc44e2c72f8d9a1
Sagyam Thapa <sagyamthapa32@gmail.com>
2023-11-26 21:34:04 +0545

    typo fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://grid-sync.vercel.app],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://grid-sync.vercel.app'],
   });
   app.useGlobalPipes(
     new ValidationPipe({
