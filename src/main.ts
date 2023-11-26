39df255abb09572049319522548bae4526181c3b
Sagyam Thapa <sagyamthapa32@gmail.com>
2023-11-26 21:32:32 +0545

    🐛 fix(app): added cors url

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://grid-sync.vercel.app],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
