206a59824f81c5bc37d6b9b172479d2017085c6f
Sagyam Thapa <sagyamthapa32@gmail.com>
2023-11-26 22:52:08 +0545

    ✨ feat(server): added rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BatteryModule } from './battery/battery.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), BatteryModule],
+  imports: [
+    ConfigModule.forRoot(),
+    BatteryModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 30,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
